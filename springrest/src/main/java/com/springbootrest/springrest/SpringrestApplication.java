f6ff254fe58022762e422a8e0528403e8adcb949
vaishnavi1104 <vaishnaviikarande@gmail.com>
2024-05-23 11:56:51 +0530

    second commit

--- springrest/src/main/java/com/springbootrest/springrest/SpringrestApplication.java
+++ springrest/src/main/java/com/springbootrest/springrest/SpringrestApplication.java
@@ -8,6 +8,7 @@ public class SpringrestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringrestApplication.class, args);
-	}
+		System.out.println("Running");
 
+}
 }
